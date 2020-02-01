df6b70cf0c4e523c245eef62d1cabf24cddda713
damarrus <damarrus92@gmail.com>
2020-02-01 13:44:49 +0300

    imsgaes

---
+++ Assets/Scripts/Container/TableClicker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class TableClicker : MonoBehaviour
+{
+    public BoxCollider2D boxCollider2D;
+    public Action<Vector2> OnTableClick = delegate { };
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (boxCollider2D.OverlapPoint(Input.mousePosition))
+            {
+                OnTableClick.Invoke(Input.mousePosition);
+            }
+        }
+    }
+}
